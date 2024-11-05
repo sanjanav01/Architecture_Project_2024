495395668c1d886ef1b11385608f874438ebe5ea
Sanjana <sanjanavellanki01@gmail.com>
2024-11-06 00:32:07 +0100

    Initial Configurations (cmake, googletest)

--- cmake-build-debug/ftest-soa/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/shriyasingh/Desktop/architecture_project_2024/Architecture_Project_2024/ftest-soa
-# Build directory: /Users/shriyasingh/Desktop/architecture_project_2024/Architecture_Project_2024/cmake-build-debug/ftest-soa
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- cmake-build-debug/utest-imgaos/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/shriyasingh/Desktop/architecture_project_2024/Architecture_Project_2024/utest-imgaos
-# Build directory: /Users/shriyasingh/Desktop/architecture_project_2024/Architecture_Project_2024/cmake-build-debug/utest-imgaos
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
